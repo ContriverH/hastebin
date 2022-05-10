50a4ce86c095f13df8c393e2f260af88c9f87304
Himanshu Pal <himanshu.p19@iiits.in>
2022-05-10 22:49:15 +0530

    port value made dynamic

--- server.js
+++ server.js
@@ -64,4 +64,7 @@ app.get("/:id", async (req, res) => {
   }
 });
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, (err) => {
+  console.log("Server running on port: " + port);
+});
